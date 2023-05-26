dfdfc4ef0f333f271ab6ec31bdd43de0c97c54cc
nora-sch <nora.schumann.development@gmail.com>
2023-05-26 12:04:34 +0200

    change post 'fetch' technique from useLocation (state) to useParams (id)+fetch async from url

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,15 +1,34 @@
+import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 
 const Post = () => {
-  let { state } = useLocation();
-  console.log(state);
   const { id } = useParams();
+  const [post, setPost] = useState({});
+  useEffect(() => {
+    const getOnePost = async () => {
+      const post = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/${id}`
+      );
+      const postJson = await post.json();
+      console.log(postJson)
+      setPost(postJson);
+    };
+    getOnePost();
+  }, []);
+
   return (
     <div>
-      <h2>{state.post.title.toUpperCase()}</h2>
-      <p>{state.post.body}</p>
+      <h2 style={{textTransform:"uppercase"}}>{post.title}</h2>
+      <p>{post.body}</p>
     </div>
   );
+  //   let { state } = useLocation();
+  //   return (
+  //     <div>
+  //       <h2>{state.post.title.toUpperCase()}</h2>
+  //       <p>{state.post.body}</p>
+  //     </div>
+  //   );
 };
 
 export default Post;
